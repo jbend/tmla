4cb1130d9f4ed9074e24b738bac2f21c6d583687
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2019-12-28 19:57:45 -0800

    Removing amplify and adding some tweaks

--- src/app/components/add-link/add-link.component.ts
+++ src/app/components/add-link/add-link.component.ts
@@ -4,7 +4,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 import { Store, Select } from '@ngxs/store';
-import { ModalState, ShowAddLinkModal } from '../../shared/state';
+import { ModalState, ShowAddLinkModal, CreateLink } from '../../shared/state';
 
 @Component({
   selector: 'app-add-link',
@@ -31,20 +31,19 @@ export class AddLinkComponent implements OnInit {
   ngOnInit() {
   }
 
-  // onSubmit() {
-  //   // TODO: Use EventEmitter with form value
-  //   console.warn('onSubmit()', this.addLinkForm.value);
-  // }
-
   cancelAddApplication() {
     this.addLinkForm.reset();
-    // this.addModal = false;
     this.store.dispatch(new ShowAddLinkModal(false));
   }
 
   addApplication() {
-    // console.warn(this.addApplicationForm.value);
-    console.log('addApplicationForm', this.addLinkForm.value);
+    this.store.dispatch(new CreateLink({
+      name: this.addLinkForm.value.name,
+      description: this.addLinkForm.value.description,
+      target: this.addLinkForm.value.target,
+      linkType: this.addLinkForm.value.linkType,
+      icon: this.addLinkForm.value.icon
+    }));
     this.store.dispatch(new ShowAddLinkModal(false));
   }
 
